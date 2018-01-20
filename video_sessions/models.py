0294f8d7e14178134f65020fda8bc08f86d3f0c3
mateus <mateussnogs@gmail.com>
2018-01-20 18:28:06 -0200

    algumas restirocoes nos campos do formulario

--- video_sessions/models.py
+++ video_sessions/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
 class Feedback(models.Model):
-    incomodo = models.IntegerField()
-    interesse1 = models.IntegerField()
-    interesse2 = models.IntegerField()
-    num_sessao = models.IntegerField()
-    num_video_preferido = models.IntegerField()
+    incomodo = models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(10)])
+    interesse1 = models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(10)])
+    interesse2 = models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(10)])
+    num_sessao = models.IntegerField(validators=[MinValueValidator(1),
+                                       MaxValueValidator(10)])
+    num_video_preferido = models.IntegerField(validators=[MinValueValidator(1),
+                                       MaxValueValidator(2)])
     justificativa = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
