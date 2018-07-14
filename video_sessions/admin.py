70dd69697a79b0f731ee228042b99bfcbbb46204
mateus <mateussnogs@gmail.com>
2018-07-14 13:11:06 -0300

    tentando fazer admin page mostrar a p#@# da sessionid

--- video_sessions/admin.py
+++ video_sessions/admin.py
@@ -5,18 +5,19 @@ from .models import Feedback, StressFeedback, Categories
 
 
 class FeedbackAdmin(admin.ModelAdmin):
-
+    empty_value_display = 'Unknown Item field'
     list_display = ['num_sessao', 'num_video_preferido', 'incomodo', 'interesse1', 'interesse2', 'created_date', 'justificativa', 'email', 'session_id', 'comment']
     search_fields = ['justificativa', 'num_sessao']
     list_filter = ['created_date']
 
 class StressFeedbackAdmin(admin.ModelAdmin):
-
+    empty_value_display = 'Unknown Item field'
     list_display = ['num_sessao', 'estresse', 'justificativa', 'created_date', 'email', 'session_id']
     search_fields = ['justificativa', 'estresse']
     list_filter = ['created_date']
 
 class CategoriesAdmin(admin.ModelAdmin):
+    empty_value_display = 'Unknown Item field'
     list_display = ['comedia', 'esporte', 'documentario', 'musica', 'english', 'session_id', 'created_date']
 
 admin.site.register(Feedback, FeedbackAdmin)
